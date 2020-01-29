4aa68f8eedf21f313fafebf9aa40e119c9b1c53f
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-29 22:36:42 +0300

    override methods activity

--- app/src/main/java/com/bankmtk/lifecycleactivity/MainActivity.java
+++ app/src/main/java/com/bankmtk/lifecycleactivity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bankmtk.lifecycleactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String instanceState;
+        if (savedInstanceState ==null){
+            instanceState ="First start";
+        }
+        else {
+            instanceState = "Repeated start";
+        }
+        Toast.makeText(getApplicationContext(), instanceState+" - onCreate()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(),"OnStart()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "Repeated start!!-onRestoreInstanceState()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"onResume()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"onPause()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Toast.makeText(getApplicationContext(),"onSaveInstanceState()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(),"onStop()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(),"onRestart()",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"onDestroy()",
+                Toast.LENGTH_SHORT).show();
     }
 }
