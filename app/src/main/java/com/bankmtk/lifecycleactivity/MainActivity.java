d0d9595cc7d74c0483e2b2107d15a57a979f8f49
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-01-30 22:00:45 +0300

    refactor MainActivity

--- app/src/main/java/com/bankmtk/lifecycleactivity/MainActivity.java
+++ app/src/main/java/com/bankmtk/lifecycleactivity/MainActivity.java
@@ -1,95 +1,78 @@
 package com.bankmtk.lifecycleactivity;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK){
+            EditText num = (EditText)findViewById(R.id.editText2);
+            num.setText(data.getCharSequenceExtra("Number"));
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String instanceState;
-        if (savedInstanceState ==null){
-            instanceState ="First start";
-        }
-        else {
-            instanceState = "Repeated start";
-        }
-        Toast.makeText(getApplicationContext(), instanceState+" - onCreate()",
+        Toast.makeText(getApplicationContext(),"Main - onCreate()",
                 Toast.LENGTH_SHORT).show();
-        final TextView textcounter = (TextView)findViewById(R.id.textCounter);
-        final LifeCyclePresenter presenter = LifeCyclePresenter.getInstance();
-        textcounter.setText(((Integer)presenter.getCounter()).toString());
         Button button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                presenter.incrementCounter();
-                textcounter.setText(((Integer)presenter.getCounter()).toString());
-            }
-        });
+        button.setOnClickListener(new StartSecondActivity(this));
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(getApplicationContext(),"OnStart()",
-                Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Toast.makeText(getApplicationContext(), "Repeated start!!-onRestoreInstanceState()",
+        Toast.makeText(getApplicationContext(),"Main-OnStart()",
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(getApplicationContext(),"onResume()",
+        Toast.makeText(getApplicationContext(),"Main-onResume()",
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(getApplicationContext(),"onPause()",
-                Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Toast.makeText(getApplicationContext(),"onSaveInstanceState()",
+        Toast.makeText(getApplicationContext(),"Main-onPause()",
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(getApplicationContext(),"onStop()",
+        Toast.makeText(getApplicationContext(),"Main-onStop()",
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(getApplicationContext(),"onRestart()",
+        Toast.makeText(getApplicationContext(),"Main-onRestart()",
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(getApplicationContext(),"onDestroy()",
+        Toast.makeText(getApplicationContext(),"Main-onDestroy()",
                 Toast.LENGTH_SHORT).show();
     }
 }
